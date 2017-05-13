86ae8d4a0e8ea5dfa62ac97c4c4ea795a28e2849
Kalan <kjj6198@gmail.com>
2017-05-13 21:52:49 +0800

    adjust rent parse logic, and make some sleep time.

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ var pg = require('pg');
 
 var connect = 'postgres://localhost/rentinfo';
 
-// function sleep(ms) {
-//   return new Promise(resolve => setTimeout(resolve, ms));
-// }
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 // async function demo() {
 //   console.log('Taking a break...');
@@ -17,41 +17,44 @@ var connect = 'postgres://localhost/rentinfo';
 
 // demo();
 
-pg.connect(connectionString, onConnect);
+// pg.connect(connectionString, onConnect);
 
-function onConnect(err, client, done) {
-  //Err - This means something went wrong connecting to the database.
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
+// function onConnect(err, client, done) {
+//   //Err - This means something went wrong connecting to the database.
+//   if (err) {
+//     console.error(err);
+//     process.exit(1);
+//   }
 
-  //For now let's end client
-  client.end();
-}
+//   //For now let's end client
+//   client.end();
+// }
 
-function searchHouse(url, currentPage) {
-  console.log('parseing house page... 目前頁數為' + currentPage);
-  request(url, (err, res, body) => {
+async function searchHouse(url, currentPage) {
+  console.log('parsing house page... 目前頁數為' + currentPage);
+  request(url, async(err, res, body) => {
     var $ = cheerio.load(body);
     var totalNum = $('.pageNum-form').data('total');
 
     var allPages = +totalNum / 30;
-    if (currentPage < allPages) {
-      $('#content > .listInfo').each((i, elm) => {
-        const url = 'https:' + $(elm).find('a').attr('href').trim();
-        sleep(4000);
-        // request(url, (err, res, houseBody) => {
-          // var houseParser = new HouseParser(houseBody, url);
-          // sleep(4000);
-        // });
-        sleep(4000);
-      });
 
+    await sleep(10000);
+
+    if (currentPage < allPages) {
+      await sleep(4000);
+      $('#content > .listInfo').each(async(i, elm) => {
+        const url = 'https:' + $(elm).find('a').attr('href').trim();
+        await sleep(4000);
+        request(url, async(err, res, houseBody) => {
+          var houseParser = new HouseParser(houseBody, url);
+          console.log(houseParser.getAllInfo());
+          await sleep(4000);
+        });
+        await sleep(4000);
+      });
       searchHouse(url + '&firstRow=' + currentPage * 30 + '&totalRow=' + totalNum, ++currentPage);
-      sleep(10000);
     }
   })
 }
 
-searchHouse('https://rent.591.com.tw/new/?kind=0&region=1&section=7,5,3,4,1&rentprice=9000,13500&area=7,12', 0);
+searchHouse('https://rent.591.com.tw/new/?kind=0&region=1&section=7,5,3,4,1&rentprice=9000,13500&area=7,12', 1);
